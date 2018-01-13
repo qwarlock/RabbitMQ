f2df4e5fc04f3509cefb1d476420525c52bc2bae
Steven Zawadzski <qwarlock@qwarlock.com>
2018-01-12 21:29:34 -0800

    This is a test project for RabbitMQ.

---
+++ src/main/java/com/qwarlock/App.java
@@ -0,0 +1,13 @@
+package com.qwarlock;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
